d81617c96fd8b4b3a16d4e74914e6e7ea344ac1f
Kushagra Patidar <87438258+kushagrapatidar@users.noreply.github.com>
2025-03-10 17:30:30 +0530

    Day5
    rtu_db_assignment

---
+++ Day5/rtu_assignment.sql
@@ -0,0 +1,89 @@
+
+-- Create RTU Database
+create database rtudb;
+use rtudb;
+
+--
+-- Table Creations
+
+create table rtu(
+rtu_id varchar(10) primary key,
+location varchar(50) not null,
+ip_address varchar(15) unique not null
+);
+-- drop table rtu;
+
+
+create table users(
+user_id varchar(10) primary key,
+user_name varchar(100) not null,
+user_role varchar(20) not null
+);
+-- drop table users;
+
+create table sensors(
+sensor_id varchar(10) primary key,
+rtu_id varchar(10),
+sensor_status varchar(8) not null,
+sensor_type varchar(20) not null,
+req_val int not null,
+curr_val int,
+foreign key (rtu_id) references rtu(rtu_id)
+);
+-- drop table sensors;
+
+create table controllers(
+user_id varchar(10) not null,
+sensor_id varchar(10) not null,
+primary key (user_id,sensor_id),
+foreign key (user_id) references users(user_id),
+foreign key (sensor_id) references sensors(sensor_id)
+);
+-- drop table controllers;
+
+--
+-- Data Insertion
+
+insert into rtu values
+('R101', 'BENGALURU', '192.168.0.1'),
+('R102', 'MUMBAI',    '192.168.0.2'),
+('R103', 'CHENNAI',   '192.168.0.3'),
+('R104', 'NEW DELHI', '192.168.0.4'),
+('R105', 'KOLKATA',   '192.168.0.5');
+
+
+insert into users values
+('U501', 'PRAKASH',    'MONITOR'),
+('U502', 'PARTHIV', 'ADMINISTER'),
+('U503', 'SMITHA',  'ADMINISTER');
+
+
+insert into sensors values
+('S801','R101', 'ENABLED',  'PRESSURE',      34,   33),
+('S802','R101', 'ENABLED',  'TEMPERATURE',   98,   90),
+('S803','R101', 'DISABLED', 'TEMPERATURE',   96, null),
+('S804','R102', 'ENABLED',  'PRESSURE',      30,   32),
+('S805','R102', 'ENABLED',  'PRESSURE',      36,   32),
+('S806','R102', 'DISABLED', 'TEMPERATURE',  108, null),
+('S807','R102', 'ENABLED',  'BAROMETER',    750,  660),
+('S808','R103', 'ENABLED',  'BAROMETER',    780,  690),
+('S809','R103', 'ENABLED',  'ULTRASONIC',   510,  450),
+('S810','R104', 'DISABLED', 'ULTRASONIC',   530, null),
+('S811','R104', 'ENABLED',  'PRESSURE',      60,   50);
+
+
+insert into controllers values
+('U501','S801'),
+('U501','S804'),
+('U501','S806'),
+('U502','S802'),
+('U502','S804'),
+('U503','S805'),
+('U503','S806'),
+('U501','S809'),
+('U502','S810'),
+('U503','S811'),
+('U501','S807'),
+('U503','S808'),
+('U502','S801'),
+('U501','S808');
